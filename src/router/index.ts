fd48cb691892c67fc3198a08cc72e4ba916ea76a
Ruslans Krims <krims.ruslan@gmail.com>
2026-05-15 20:54:12 +0200

    refactor: renamed view, added redirection to "/chat" route on "/"

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ChatView from '../views/ChatView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/chat',
+    },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: ChatView,
     },
   ],
 })
